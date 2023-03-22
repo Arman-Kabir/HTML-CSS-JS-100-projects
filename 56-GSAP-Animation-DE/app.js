8b331b2e1e68b5b9b1b71a2ea0de235852e6699c
Arman-Kabir <armankabir8@gmail.com>
2023-03-22 08:14:03 +0600

    timeline fromTo

--- 56-GSAP-Animation-DE/app.js
+++ 56-GSAP-Animation-DE/app.js
@@ -10,14 +10,23 @@ const navOpen = document.querySelector('.nav-open');
 const tl = new TimelineLite({ paused: true });
 
 tl.to('.cover', 1, {
-    width:'60%',
-    ease:Power2.easeOut
+    width: '60%',
+    ease: Power2.easeOut
 })
-.to('nav',1,{
-    height:'100%',
-    ease:Power2.easeOut
-});
+    .to('nav', 1, {
+        height: '100%',
+        ease: Power2.easeOut
+    }, '-=1'
+    )
+    .fromTo('.nav-open', 0.5, {
+        opacity: 0,
+        x: 500,
+        ease: Power2.easeOut
+    }, {
+        opacity: 1,
+        x: 0
+    })
 
-navButton.addEventListener('click',()=>{
+navButton.addEventListener('click', () => {
     tl.play();
 });
