90bcd45f1bb127dad0431517ad623ff50aea05c5
Arman-Kabir <armankabir8@gmail.com>
2023-04-06 14:31:33 +0600

    scrillT start/end

--- 58-GSAP ScrollTrigger/app.js
+++ 58-GSAP ScrollTrigger/app.js
@@ -5,5 +5,11 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.to(".square", {
     x: 700,
     duration:3,
-    scrollTrigger:".square2",
+    // scrollTrigger:".square2",
+    scrollTrigger:{
+        trigger:".square",
+        // start:400,
+        start:"top 40%",
+        markers:true
+    },
 })
