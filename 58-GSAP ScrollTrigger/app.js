5427d0fed82a39ae1ffb536243c981b834198f56
Arman-Kabir <armankabir8@gmail.com>
2023-04-07 22:27:55 +0600

    doing pin

--- 58-GSAP ScrollTrigger/app.js
+++ 58-GSAP ScrollTrigger/app.js
@@ -25,25 +25,44 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 // vdo-3 ==> Toggle Actions  vdo-4 ==> scrub
+// gsap.to(".square", {
+//     x: 1000,
+//     duration: 8,
+//     scrollTrigger: {
+//         trigger: ".square",
+//         // start: "top 60%",
+//         start: "top 80%",
+//         end: "top 30%",
+//         scrub: 4,
+//         // toggleActions --> play restart pause resume complete none, 
+//         toggleActions: "restart none resume complete",
+//         //            onEnter  onLeave   onEnterBack   onLeaveBack
+
+
+//         markers: {
+//             startColor: "purple",
+//             endColor: "fuchsia",
+//             fontSize: "3rem",
+//             // indent:200,
+//         },
+//         toggleClass: "red",
+//     },
+// })
+
+
+// vdo-->5
 gsap.to(".square", {
     x: 1000,
     duration: 8,
     scrollTrigger: {
         trigger: ".square",
-        // start: "top 60%",
-        start: "top 80%",
         end: "top 30%",
         scrub: 4,
-        // toggleActions --> play restart pause resume complete none, 
         toggleActions: "restart none resume complete",
-        //            onEnter  onLeave   onEnterBack   onLeaveBack
-
-
         markers: {
             startColor: "purple",
             endColor: "fuchsia",
             fontSize: "3rem",
-            // indent:200,
         },
         toggleClass: "red",
     },
