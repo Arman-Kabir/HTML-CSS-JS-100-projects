305aaf23317c222d8c3de5925da9baa4d668f72a
Arman-Kabir <armankabir8@gmail.com>
2023-04-07 20:29:29 +0600

    scrillT toggleActions

--- 58-GSAP ScrollTrigger/app.js
+++ 58-GSAP ScrollTrigger/app.js
@@ -1,24 +1,47 @@
 
 gsap.registerPlugin(ScrollTrigger);
 
+// video -> 1 & 2
+// gsap.to(".square", {
+//     // x: 500,
+//     duration: 3,
+//     // scrollTrigger: ".square2",
+//     scrollTrigger: {
+//         trigger: ".square",
+//         // start:400,
+//         start: "top 40%",
+//         // end:"center 20%",
+//         end:()=>`+=${document.querySelector(".square").offsetHeight}`,
+//         markers: true,
+//         // markers: {
+//         //     startColor:"purple",
+//         //     endColor:"fuchsia",
+//         //     fontSize:"4rem",
+//         //     indent:200,
+//         // },
+//         toggleClass:"red",
+//     },
+// })
 
+
+// vdo-3 ==> Toggle Actions
 gsap.to(".square", {
-    // x: 500,
-    duration: 3,
-    // scrollTrigger: ".square2",
+    x: 1000,
+    duration: 8,
     scrollTrigger: {
         trigger: ".square",
-        // start:400,
-        start: "top 40%",
-        // end:"center 20%",
-        end:()=>`+=${document.querySelector(".square").offsetHeight}`,
-        markers: true,
-        // markers: {
-        //     startColor:"purple",
-        //     endColor:"fuchsia",
-        //     fontSize:"4rem",
-        //     indent:200,
-        // },
+        start: "top 60%",
+        end:"top 40%",
+        // toggleActions --> play restart pause resume complete none, 
+        toggleActions:"restart pause resume complete",
+        //            onEnter  onLeave   onEnterBack   onLeaveBack
+
+        markers: {
+            startColor:"purple",
+            endColor:"fuchsia",
+            fontSize:"3rem",
+            // indent:200,
+        },
         toggleClass:"red",
     },
 })
