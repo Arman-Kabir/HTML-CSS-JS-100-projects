7a1dbaa2d83e0569d9a77a50901002b50374f5ad
Arman-Kabir <armankabir8@gmail.com>
2023-04-07 20:10:02 +0600

    scrillT start/end done

--- 58-GSAP ScrollTrigger/app.js
+++ 58-GSAP ScrollTrigger/app.js
@@ -3,13 +3,22 @@ gsap.registerPlugin(ScrollTrigger);
 
 
 gsap.to(".square", {
-    x: 700,
-    duration:3,
-    // scrollTrigger:".square2",
-    scrollTrigger:{
-        trigger:".square",
+    // x: 500,
+    duration: 3,
+    // scrollTrigger: ".square2",
+    scrollTrigger: {
+        trigger: ".square",
         // start:400,
-        start:"top 40%",
-        markers:true
+        start: "top 40%",
+        // end:"center 20%",
+        end:()=>`+=${document.querySelector(".square").offsetHeight}`,
+        markers: true,
+        // markers: {
+        //     startColor:"purple",
+        //     endColor:"fuchsia",
+        //     fontSize:"4rem",
+        //     indent:200,
+        // },
+        toggleClass:"red",
     },
 })
