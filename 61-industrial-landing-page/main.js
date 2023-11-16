870eaf151777aad7a55b206ba00dc388a6f013ec
Arman-Kabir <armankabir8@gmail.com>
2023-11-17 00:23:48 +0600

    scrollReveal library used at hero section

---
+++ 61-industrial-landing-page/main.js
@@ -0,0 +1,15 @@
+const scrollRevealOption={
+    distance:"50px",
+    origin:"bottom",
+    duration:1000
+};
+
+// header container
+ScrollReveal().reveal(".header__container h1",{
+    ...scrollRevealOption
+});
+
+ScrollReveal().reveal(".header__container .btn",{
+    ...scrollRevealOption,
+    delay:500
+})
