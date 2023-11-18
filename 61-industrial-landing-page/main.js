82c5fe00cb9009c30bbb42dc1806c137a5a4d769
Arman-Kabir <armankabir8@gmail.com>
2023-11-18 09:40:08 +0600

    About Us items designed and animation done

--- 61-industrial-landing-page/main.js
+++ 61-industrial-landing-page/main.js
@@ -12,4 +12,10 @@ ScrollReveal().reveal(".header__container h1",{
 ScrollReveal().reveal(".header__container .btn",{
     ...scrollRevealOption,
     delay:500
-})
+});
+
+// about container
+ScrollReveal().reveal(".about__item",{
+    ...scrollRevealOption,
+    interval:500,
+});
