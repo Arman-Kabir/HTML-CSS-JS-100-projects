f51163c0526066eb5270f04440af9004dcff0850
Arman-Kabir <armankabir8@gmail.com>
2023-12-16 18:01:55 +0600

    basic html elemets and quizdata js done

---
+++ 62-dynamic-quiz/quiz.js
@@ -0,0 +1,30 @@
+// Quiz data
+
+const quizData = [
+    {
+        question:"What does HTML stand for?",
+        options:[
+            "Hypertext1",
+            "Hypertext2",
+            "Hypertext3",
+            "Hypertext4"
+        ],
+        correct:0
+    },
+    {
+        question:"Which css property is used to control spacing btn elements?",
+
+        options:['margin','padding','spacing','border-spacing'],
+        correct:1,
+
+    },
+    {
+        question:"Which css property is used to control spacing btn elements and all?",
+
+        options:['margin','padding','spacing','border-spacing'],
+        correct:1,
+
+    }
+];
+
+// js initialization
